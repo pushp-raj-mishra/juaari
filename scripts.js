4e754777469bd0e5777eeb71cab11b1924e3c605
pushp-raj-mishra <69637880+pushp-raj-mishra@users.noreply.github.com>
2025-05-18 12:22:33 +0530

    Add logic to generate buttons

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 let grid = document.querySelector(".grid");
-let size = 5;
+let size = 10;
 let bomb = [];
 for(let i = 0; i < size*size; i++){
     bomb.push(0);
@@ -17,17 +17,35 @@ for(let i = 0; i < bombNumber; i++){
     bomb[x] = 1;
 }
 
+let bmb = 0;
+let mul = 0;
+
+
+function explode(){
+    bmb++;
+    console.log(bmb);
+    console.log(mul);
+}
+function multiply(){
+    mul++;
+    console.log(bmb);
+    console.log(mul);
+}
 
 let ptr = 0;
 for(let i = 0; i < size; i++){
+    let bigDabba = document.createElement("div");
     for(let j =0; j<size;j++){
         let dabba = document.createElement("button");
+        dabba.textContent = "Click";
         if (bomb[ptr]==1) {
-            dabba.addEventListener("click",explode());
+            dabba.addEventListener("click",explode);
         } else {
-            dabba.addEventListener("click",multiply());
+            dabba.addEventListener("click",multiply);
         }
-        grid.appendChild(dabba);
+        bigDabba.appendChild(dabba);
+        
         ptr++;
     }
+    grid.appendChild(bigDabba);
 }
