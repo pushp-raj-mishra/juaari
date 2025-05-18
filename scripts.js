e3b61965d03634a3244435e61de10e7ca6042b4a
pushp-raj-mishra <69637880+pushp-raj-mishra@users.noreply.github.com>
2025-05-18 12:06:49 +0530

    Add individual cell creation and bomb creation

--- scripts.js
+++ scripts.js
@@ -1,2 +1,33 @@
 
 let grid = document.querySelector(".grid");
+let size = 5;
+let bomb = [];
+for(let i = 0; i < size*size; i++){
+    bomb.push(0);
+}
+
+
+let bombNumber = ((size*size*25)/100);
+
+for(let i = 0; i < bombNumber; i++){
+    let x = Math.ceil(Math.random()*100)+1;
+    while (bomb[x]!=0) {
+        x = Math.ceil(Math.random()*100)+1;
+    }
+    bomb[x] = 1;
+}
+
+
+let ptr = 0;
+for(let i = 0; i < size; i++){
+    for(let j =0; j<size;j++){
+        let dabba = document.createElement("button");
+        if (bomb[ptr]==1) {
+            dabba.addEventListener("click",explode());
+        } else {
+            dabba.addEventListener("click",multiply());
+        }
+        grid.appendChild(dabba);
+        ptr++;
+    }
+}
